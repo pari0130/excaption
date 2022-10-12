080217f8c77e6414111f33454398cc9842b9ac3f
dhcho-alticast <85541792+dhcho-alticast@users.noreply.github.com>
2022-10-12 16:31:22 +0900

    에러 처리 보완

--- src/main/kotlin/com/example/excaption/GlobalException.kt
+++ src/main/kotlin/com/example/excaption/GlobalException.kt
@@ -3,6 +3,7 @@ package com.example.excaption
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.web.WebProperties
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler
+import org.springframework.boot.web.error.ErrorAttributeOptions
 import org.springframework.boot.web.reactive.error.ErrorAttributes
 import org.springframework.context.ApplicationContext
 import org.springframework.core.annotation.Order
@@ -14,6 +15,7 @@ import org.springframework.web.reactive.function.server.RequestPredicates
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.RouterFunctions
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 
 @Component
@@ -44,12 +46,33 @@ class GlobalException(
         }
     }
 
-    private fun badResponse(error: Throwable): Mono<ServerResponse> {
-        return ServerResponse.status(HttpStatus.OK).body(
-            BodyInserters.fromValue(
-                Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), error.message.toString(), null)
-            )
-        )
+//    private fun badResponse(error: Throwable): Mono<ServerResponse> {
+//        val errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults())
+//        val status = Optional.ofNullable(errorAttributes["status"]).orElse(500) as Int
+//
+//        return ServerResponse.status(HttpStatus.OK).body(
+//            BodyInserters.fromValue(
+//                Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), error.message.toString(), null)
+//            )
+//        )
+//    }
+
+    private fun badResponse(throwable: Throwable): Mono<ServerResponse> {
+        return when (throwable) {
+            is ResponseStatusException -> {
+                logger.info("getRoutingFunction error {} {} ", throwable.status, throwable.rawStatusCode)
+                ServerResponse.status(throwable.rawStatusCode)
+                    .body(
+                        BodyInserters.fromValue(
+                            Response(throwable.rawStatusCode, throwable.message.toString(), null)))
+            }
+            else -> {
+                ServerResponse.status(HttpStatus.OK)
+                    .body(
+                        BodyInserters.fromValue(
+                            Response(HttpStatus.INTERNAL_SERVER_ERROR.value(), throwable.message.toString(), null)))
+            }
+        }
     }
 
     private fun renderErrorResponse(error: CustomException): Mono<ServerResponse> {
